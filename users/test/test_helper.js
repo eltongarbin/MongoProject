5e54ee532672c1a90faa4f07a3efbf1e373d3095
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-03-07 20:57:06 -0300

    Thinking About Schema Design

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -2,15 +2,19 @@ const mongoose = require('mongoose');
 
 before((done) => {
   mongoose.connect('mongodb://localhost/users_test');
-  mongoose.connection
-    .once('open', () => done())
-    .on('error', (error) => {
-      console.warn('Warning', error);
-    });
+  mongoose.connection.once('open', () => done()).on('error', (error) => {
+    console.warn('Warning', error);
+  });
 });
 
 beforeEach((done) => {
-  mongoose.connection.collections.users.drop(() => {
-    done();
+  const { users, comments, blogposts } = mongoose.connection.collections;
+
+  users.drop(() => {
+    comments.drop(() => {
+      blogposts.drop(() => {
+        done();
+      });
+    });
   });
 });
