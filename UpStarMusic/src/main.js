36eed516ce7a71cb91c11605bffd7b00263abd20
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-03-21 21:18:18 -0300

    new project - initial commit

---
+++ UpStarMusic/src/main.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import { MongoClient } from 'mongodb';
+import reducers from './reducers';
+import Routes from './router';
+import mongoose from 'mongoose';
+import './seeds';
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'upstar_music';
+
+mongoose.Promise = Promise;
+
+const App = () => {
+  const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+  return (
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  );
+};
+
+MongoClient.connect(url, function(err, client) {
+  window.db = client.db(dbName);
+
+  mongoose.connect('mongodb://localhost/upstar_music');
+  mongoose.connection
+    .once('open', () => {
+      ReactDOM.render(<App />, document.getElementById('root'));
+    })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
---
+++ UpStarMusic/src/router.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import ArtistMain from './components/artists/ArtistMain';
+import ArtistDetail from './components/artists/ArtistDetail';
+import ArtistCreate from './components/artists/ArtistCreate';
+import ArtistEdit from './components/artists/ArtistEdit';
+
+const Routes = () => {
+  return (
+    <HashRouter>
+      <div className="container">
+        <Header />
+        <Route exact path="/" component={ArtistMain} />
+        <Switch>
+          <Route path="artists/new" component={ArtistCreate} />
+          <Route path="artists/:id" component={ArtistDetail} />
+          <Route path="artists/:id/edit" component={ArtistEdit} />
+        </Switch>
+      </div>
+    </HashRouter>
+  );
+};
+
+export default Routes;
