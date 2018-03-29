b7076704e2b628c8b551297e045de66abf156d98
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-03-28 21:49:43 -0300

    muber - Middlewares in Express

--- muber/app.js
+++ muber/app.js
@@ -13,4 +13,8 @@ const routes = require('./routes/routes');
 app.use(bodyParser.json());
 routes(app);
 
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err._message });
+});
+
 module.exports = app;
